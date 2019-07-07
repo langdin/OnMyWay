44fdc6fdef28142eb0f0ea240086f44ae45b2e7c
Din Lanh <langdinhieu@gmail.com>
2019-07-06 22:35:08 -0400

    first commit

---
+++ app/src/main/java/com/example/onmyway/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.onmyway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(7000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+
+                }
+            }
+        };
+    }
+}
