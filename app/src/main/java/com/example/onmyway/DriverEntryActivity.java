dd9b046d92ca2a1cdd2ad1e9d9af60013a996b6d
Din Lanh <langdinhieu@gmail.com>
2019-07-07 22:17:10 -0400

    added customer and driver entry activity

---
+++ app/src/main/java/com/example/onmyway/DriverEntryActivity.java
@@ -0,0 +1,54 @@
+package com.example.onmyway;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DriverEntryActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnRegister;
+    private TextView lblGoToRegister;
+    private TextView lblMain;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_entry);
+
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+        btnRegister = (Button)findViewById(R.id.btnRegister);
+        lblGoToRegister = (TextView) findViewById(R.id.lblGoToRegister);
+        lblMain = (TextView) findViewById(R.id.lblMainLbl);
+
+        btnLogin.setEnabled(true);
+        btnLogin.setVisibility(View.VISIBLE);
+
+        btnRegister.setVisibility(View.INVISIBLE);
+        btnRegister.setEnabled(false);
+    }
+
+    public void goToRegister(View view) {
+        btnLogin.setVisibility(View.INVISIBLE);
+        btnLogin.setEnabled(false);
+
+        btnRegister.setEnabled(true);
+        btnRegister.setVisibility(View.VISIBLE);
+
+        lblGoToRegister.setVisibility(View.INVISIBLE);
+        lblGoToRegister.setEnabled(false);
+
+        lblMain.setText("Customer Register");
+
+    }
+
+    public void login(View view) {
+
+    }
+
+    public void register(View view) {
+
+    }
+}
--- app/src/main/java/com/example/onmyway/WelcomeActivity.java
+++ app/src/main/java/com/example/onmyway/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.onmyway;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    public void goToCustomerEntry(View view) {
+        goToActivity(true);
+    }
+
+    public void goToDriverEntry(View view) {
+        goToActivity(false);
+    }
+
+    public void goToActivity(boolean act) {
+        Intent i;
+        if(act) {
+            i = new Intent(WelcomeActivity.this, CustomerEntryActivity.class);
+        } else {
+            i = new Intent(WelcomeActivity.this, DriverEntryActivity.class);
+        }
+        startActivity(i);
+
     }
 }
